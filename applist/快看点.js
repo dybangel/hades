158037081a9fa57880111fbd3949c443ba20d2b9
ProbieMrX <648131813@qq.com>
2019-07-18 21:57:10 +0800

    修正7个APP的json和js文件

--- applist/快看点.js
+++ applist/快看点.js
@@ -1,45 +1,25 @@
-module.exports = {
-    //用于检索非广告的新闻条目
-    finditem:function(){
-
-
-    /******************************自定义部分开始********************************************/    
-  //趣看点
+function finditem(){  
             //1标识出主框架定界符
-            var ele=className(v7feature);//.className("LinearLayout").findOnce(5);
+            var ele=className(v7feature);
             //2定位到结构块层级父节点，并取出结构块数量
             var subcount=ele.findOnce(0).childCount();
-            //检测一下子节点数量是否正确
-            //alert(subcount);exit();
             //将主框架实例化
             var main=ele.findOnce(0);
             for(var i=1;i<subcount;i++){
                   try{
-                       //取出标题，主要是为了验证正确性
-                      var ltitle=main.child(i).child(0).text();
-                       //alert(ltitle);
-                       
-                       // if(main.child(i).child(0).child(0).child(0).child(1).id()=="adRightImageView"){
-                        if(main.child(i).child(0).child(0).childCount()==1){
-                    
+                       var gg1=main.child(i).child(0).child(0);
+                        if(gg1.childCount()==1){
                                 play("global",i);
                                 play("global","点击");
                                 return main.child(i);
-                       }else{ 
-                            play("global",i);
-                            play("global","点击"); 
-                            return main.child(i);
                        }
-                        
                  }catch(e){
-            
-                     play("global",i);
-                   play("global","点击")
-                   return main.child(i);
-
                   }
+                  try{
+                    play('global',i);
+                    play('global','点击');
+                    return main.child(i);
+                    }catch(e){
+                    }
             }
-
-/******************************自定义部分结束********************************************/ 
     }
-}
