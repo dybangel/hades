c9925abbeac4df8f148c25f543a6f9015a25f9b6
hubGeGe <1205939666@qq.com>
2019-07-23 00:20:26 +0800

    红包视频改为广告全部不点击

--- applist/红包视频.js
+++ applist/红包视频.js
@@ -1,4 +1,4 @@
-function finditem(){ 
+function finditem(){
             var ele=className(v7feature);
             var subcount=ele.findOnce(0).childCount();
             var main=ele.findOnce(0);
@@ -14,8 +14,8 @@ function finditem(){
                     }
                 try{
                     play('global',i);
-                    play('global','点击');
-                    return main.child(i);
+                    play('global','广告不点击');
+                    return false;
                     }catch(e){
                     }
                 }
