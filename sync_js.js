db9e86b5dc042ceebf129a936fe610a2a050ba91
dybangel <176682769@qq.com>
2019-07-22 23:12:48 +0800

    增加开发人员脚本，sync_js.js用于同步git代码到手机

---
+++ sync_js.js
@@ -0,0 +1,18 @@
+toast("同步js中......");
+var r=http.get("https://raw.githubusercontent.com/dybangel/hades/master/main_noui.js");
+var tmpstr=r.body.string();
+files.write("main_noui.js",tmpstr);
+
+var r=http.get("https://raw.githubusercontent.com/dybangel/hades/master/myswipe.js");
+var tmpstr=r.body.string();
+files.write("myswipe.js",tmpstr);
+
+var r=http.get("https://raw.githubusercontent.com/dybangel/hades/master/mycommon.js");
+var tmpstr=r.body.string();
+files.write("mycommon.js",tmpstr);
+//appver_check.js
+var r=http.get("https://raw.githubusercontent.com/dybangel/hades/master/appver_check.js");
+var tmpstr=r.body.string();
+files.write("appver_check.js",tmpstr);
+
+alert("老铁！同步成功！\n  main_noui.js \n myswipe.js \n mycommon.js \n appver_check.js");
