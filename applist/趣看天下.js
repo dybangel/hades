0a3d98a1dc1b316dd3bf3ee51f9718ad31a16f91
hubGeGe <1205939666@qq.com>
2019-08-01 21:38:52 +0800

    趣看天下，快看点，淘新闻的back机制

--- applist/趣看天下.js
+++ applist/趣看天下.js
@@ -2,7 +2,7 @@ function  finditem(){
         var ele=className(v7feature);
         var subcount=ele.findOnce(0).childCount();
         var main=ele.findOnce(0);
-        for(var i=0;i<subcount;i++){
+        for(var i=1;i<subcount;i++){
             try{
                 play('global',i);
                 play('global','点击');
