772979d2e3f7b6d1c6c5fa0823d266b967a94f29
hubGeGe <1205939666@qq.com>
2019-08-02 20:12:08 +0800

    新增盈贝头条和淘最热点json文件

--- applist/淘最热点.js
+++ applist/淘最热点.js
@@ -1,10 +1,10 @@
 function finditem(){
-    var ele=className("android.view.ViewGroup");
+    var ele=className(v7feature);
     var subcount=ele.findOnce(0).childCount();
     var main=ele.findOnce(0);
-    for(var i=2;i<subcount;i++){
+    for(var i=1;i<subcount;i++){
         try{
-            var gg1=main.child(i).child(2).child(4);
+            var gg1=main.child(i).child(0).child(0).child(1);
             if(gg1.text()=='广告'){
                 play('global',i);
                 play('global','广告不点击'); 
@@ -13,17 +13,8 @@ function finditem(){
         }catch(e){
             }
         try{
-            var gg2=main.child(i).child(2).child(3);
-            if(gg2.text()=='广告'){
-                play('global',i);
-                play('global','广告不点击'); 
-                return false;
-            }
-        }catch(e){
-            }
-        try{
-            var gg3=main.child(i).child(1).child(4);
-            if(gg3.text()=='广告'){
+            var gg2=main.child(i).child(0).child(0).child(0).child(0).child(0).child(0).child(0);
+            if(gg2.childCount()==1){
                 play('global',i);
                 play('global','广告不点击'); 
                 return false;
