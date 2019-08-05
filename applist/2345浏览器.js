80569cb74987e65f22a00cd9a92ce54c00b05e1d
ProbieMrX <648131813@qq.com>
2019-08-05 23:42:32 +0800

    修正23

--- applist/2345浏览器.js
+++ applist/2345浏览器.js
@@ -13,6 +13,15 @@ function finditem(){
                 }catch(e){
                 }
                 try{
+                    var gg2=main.child(i).child(2).child(0);
+                    if(gg2.text()=="广告"){
+                                play("global",i);
+                                play("global","广告不点击");
+                                  return false;
+                    }
+                }catch(e){
+                }
+                try{
                     play('global',i);
                     play('global','点击');
                     return main.child(i);
