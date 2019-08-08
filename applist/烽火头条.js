aebf38d800a3370d572177253616b8c51a64a4d9
hubGeGe <1205939666@qq.com>
2019-08-08 20:32:06 +0800

    -m烽火头条 趣看天下json和js文件添加

--- applist/烽火头条.js
+++ applist/烽火头条.js
@@ -3,6 +3,15 @@ function finditem(){
     var subcount=ele.findOnce(0).childCount();
     var main=ele.findOnce(0);
     for(var i=1;i<subcount;i++){
+        try{
+            var gg1=main.child(i);
+            if("android.widget.LinearLayout"==gg1.className()){
+                     play("global",i);
+                     play("global","广告不点击");
+                     return false;
+            } 
+        }catch(e){
+        }
            try{
             play('global',i);
             play('global','点击');
--- applist/趣看天下.js
+++ applist/趣看天下.js
@@ -3,6 +3,33 @@ function  finditem(){
         var subcount=ele.findOnce(0).childCount();
         var main=ele.findOnce(0);
         for(var i=1;i<subcount;i++){
+         try{
+            var gg1=main.child(i);
+            if("广告"==gg1.child(4).child(0).text()){
+                        play("global",i);
+                        play("global","广告不点击");
+                        return false;
+                } 
+          }catch(e){
+           }
+        try{
+            var gg2=main.child(i);
+            if("广告"==gg2.child(5).text()){
+                     play("global",i);
+                     play("global","广告不点击");
+                     return false;
+            } 
+            }catch(e){
+            }
+         try{
+                var gg2=main.child(i);
+                if("广告"==gg2.child(7).text()){
+                         play("global",i);
+                         play("global","广告不点击");
+                         return false;
+                } 
+                }catch(e){
+                }
             try{
                 play('global',i);
                 play('global','点击');
