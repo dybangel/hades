8fe555a3bb42dc619f1a4f21bac5b562840ad4cd
hubGeGe <1205939666@qq.com>
2019-08-09 18:29:37 +0800

    盈贝头条添加广告

--- applist/盈贝头条.js
+++ applist/盈贝头条.js
@@ -1,10 +1,10 @@
 function finditem(){
-    var ele=className("android.view.ViewGroup");
+    var ele=className(androidx);
     var subcount=ele.findOnce(0).childCount();
     var main=ele.findOnce(0);
     for(var i=1;i<subcount;i++){
         try{
-            var gg1=main.child(i).child(2).child(4);
+            var gg1=main.child(i).child(4);
             if(gg1.text()=='广告'){
                 play('global',i);
                 play('global','广告不点击'); 
@@ -13,17 +13,8 @@ function finditem(){
         }catch(e){
             }
         try{
-            var gg2=main.child(i).child(2).child(3);
+            var gg2=main.child(i).child(1).child(1);
             if(gg2.text()=='广告'){
-                play('global',i);
-                play('global','广告不点击'); 
-                return false;
-            }
-        }catch(e){
-            }
-        try{
-            var gg3=main.child(i).child(1).child(4);
-            if(gg3.text()=='广告'){
                 play('global',i);
                 play('global','广告不点击'); 
                 return false;
