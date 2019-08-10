e0fa3708ceac8fc0da7d68588f35a806dbc2d424
ProbieMrX <648131813@qq.com>
2019-08-10 20:28:05 +0800

    修正有看头的json文件和js文件

---
+++ applist/二头条.js
@@ -0,0 +1,31 @@
+function  finditem(){ 
+    var ele=className(v7feature);
+    var subcount=ele.findOnce(0).childCount();
+    var main=ele.findOnce(0);
+    for(var i=1;i<subcount;i++){
+        try{
+            var gg1=main.child(i).child(0).child(0).child(0).child(0).child(0).child(2);
+            if(gg1.text()=="广告"){
+                play('global',i);
+                play('global','广告不点击'); 
+                return false;
+            }
+        }catch(e){
+            }
+        try{
+            var gg2=main.child(i).child(0).child(0).child(0).child(0).child(0).child(3);
+            if(gg2.text()=="广告"){
+                play('global',i);
+                play('global','广告不点击'); 
+                return false;
+            }
+        }catch(e){
+            }
+        try{
+            play('global',i);
+            play('global','点击');
+            return main.child(i);
+            }catch(e){
+            }
+        }
+}
