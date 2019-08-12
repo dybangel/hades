cc28836ccbdd0450ec134f97bc614ab2a6ee3fa4
ProbieMrX <648131813@qq.com>
2019-08-12 16:45:35 +0800

    修正引力资讯json文件和js文件，增加趣看视界json文件和js文件

---
+++ applist/趣看视界.js
@@ -0,0 +1,31 @@
+function  finditem(){
+    var ele=className(v7feature);
+    var subcount=ele.findOnce(0).childCount();
+    var main=ele.findOnce(0);
+    for(var i=1;i<subcount;i++){
+        try{
+            var gg1=main.child(i).child(0).child(0).child(2);
+            if(gg1.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            play('global',i);
+            play('global','广告不点击'); 
+            return false;
+                }
+            }catch(e){
+                } 
+        try{
+            var gg2=main.child(i).child(0).child(0).child(1);
+            if(gg2.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            play('global',i);
+            play('global','广告不点击'); 
+            return false;
+                }
+            }catch(e){
+                } 
+        try{
+            play('global',i);
+            play('global','点击');
+            return main.child(i);
+            }catch(e){
+            }  
+    }
+}
