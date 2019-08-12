516d506b97ba7036cd5b10cf1a54f4216a51260d
ProbieMrX <648131813@qq.com>
2019-08-13 01:55:00 +0800

    修正趣看视界，增加鸡冻头条和魔方看点的json文件和js文件

--- applist/魔方看点.js
+++ applist/魔方看点.js
@@ -1,11 +1,11 @@
 function  finditem(){
-    var ele=className(v7feature);
+    var ele=className(androidx);
     var subcount=ele.findOnce(0).childCount();
     var main=ele.findOnce(0);
     for(var i=1;i<subcount;i++){
         try{
-            var gg1=main.child(i).child(0).child(0).child(2);
-            if(gg1.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            var gg1=main.child(i).child(5);
+            if(gg1.text()=="广告"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
@@ -13,32 +13,32 @@ function  finditem(){
             }catch(e){
                 } 
         try{
-            var gg2=main.child(i).child(0).child(0).child(1);
-            if(gg2.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            var gg2=main.child(i).child(6);
+            if(gg2.text()=="广告"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
                 }
             }catch(e){
-                } 
+                }
         try{
-            var gg3=main.child(i).child(1).child(2);
-            if(gg3.id()=="com.zhangku.qukandian:id/ad_texts"){
+            var gg3=main.child(i).child(4);
+            if(gg3.text()=="广告"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
                 }
             }catch(e){
-                } 
+                }  
         try{
-            var gg4=main.child(i).child(0).child(0).child(1).child(0);
-            if(gg4.id()=="com.zhangku.qukandian:id/bottomRedIv"){
+            var gg4=main.child(i);
+            if(gg4.childCount()==2){
             play('global',i);
             play('global','广告不点击'); 
             return false;
                 }
             }catch(e){
-                } 
+                }  
         try{
             play('global',i);
             play('global','点击');
--- applist/鸡冻头条.js
+++ applist/鸡冻头条.js
@@ -4,8 +4,8 @@ function  finditem(){
     var main=ele.findOnce(0);
     for(var i=1;i<subcount;i++){
         try{
-            var gg1=main.child(i).child(0).child(0).child(2);
-            if(gg1.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            var gg1=main.child(i).child(0);
+            if(gg1.text()=="你知道么？企业招聘要查征信了！"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
@@ -13,8 +13,8 @@ function  finditem(){
             }catch(e){
                 } 
         try{
-            var gg2=main.child(i).child(0).child(0).child(1);
-            if(gg2.id()=="com.zhangku.qukandian:id/ad_one_text"){
+            var gg2=main.child(i).child(0);
+            if(gg2.text()=="网红女神教你每天收入500~3000"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
@@ -22,8 +22,8 @@ function  finditem(){
             }catch(e){
                 } 
         try{
-            var gg3=main.child(i).child(1).child(2);
-            if(gg3.id()=="com.zhangku.qukandian:id/ad_texts"){
+            var gg3=main.child(i).child(0);
+            if(gg3.text()=="男人怎么可以更长久试下这六个步骤！"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
@@ -31,8 +31,17 @@ function  finditem(){
             }catch(e){
                 } 
         try{
-            var gg4=main.child(i).child(0).child(0).child(1).child(0);
-            if(gg4.id()=="com.zhangku.qukandian:id/bottomRedIv"){
+            var gg4=main.child(i).child(0);
+            if(gg4.text()=="点击一下，让你了解自己的真实信用额度"){
+            play('global',i);
+            play('global','广告不点击'); 
+            return false;
+                }
+            }catch(e){
+                } 
+        try{
+            var gg5=main.child(i).child(0);
+            if(gg5.text()=="想用百分百的信用给你最安心的爱"){
             play('global',i);
             play('global','广告不点击'); 
             return false;
