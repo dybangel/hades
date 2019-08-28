d78dc993cbe7fcf1d74ea9248157783a26181b36
hubGeGe <1205939666@qq.com>
2019-08-29 01:58:47 +0800

    幸运APP上传

---
+++ applist/幸运APP.js
@@ -0,0 +1,69 @@
+//遍历刮奖新闻块
+function finditem(){
+         var main=className("android.support.v7.widget.RecyclerView").findOnce(0)//.childCount();
+          //alert(main.childCount());
+          for(var i=0;i<main.childCount();i++){
+            try{
+
+           ggk = main.child(i).child(0).child(5).text();
+            if("刮刮卡"==ggk ){
+            //其次判断main.child(i)的中心点是否在可视区内
+                main.child(i).click();  //控件支持clickenable true
+                //alert("点击"+ggk);
+                break;
+              }else{
+                //如果没有匹配到可以刮卡的区域，上滑一次
+                Swipe(400,1000,400,400,900);
+                //计数器增加1
+                Callback_finditem_swipecount+=1;
+                // toast("Callback_finditem_swipecount"+Callback_finditem_swipecount);
+                  }
+        }catch(e){
+            //如果出现终极异常，保持上滑一次 计数器+1
+            Swipe(400,1000,400,400,900);
+            Callback_finditem_swipecount+=1;
+         //   toast("final e:"+e)
+        }
+        if(Callback_finditem_swipecount>10){
+            //告诉主线程，没有可以刮奖卡片了，可以提前切换下一个
+            toast("通知主线程，提前结束");
+            Grunbreak=true;
+        }
+      }//for end;
+      thisforstart=false;
+      }
+//遍历刮奖新闻块结束
+//判断二级页面打开成功与否并开始滑动
+function swipeguagua(){
+  var result=id("com.wangniu.lucky:id/scratch_card_grid").exists();
+  if(result){
+    var thisleft=id("com.wangniu.lucky:id/scratch_card_grid").findOnce().bounds().left;
+    var thistop=id("com.wangniu.lucky:id/scratch_card_grid").findOnce().bounds().top;
+    var thisright=id("com.wangniu.lucky:id/scratch_card_grid").findOnce().bounds().right;
+    var thisbottom=id("com.wangniu.lucky:id/scratch_card_grid").findOnce().bounds().bottom;
+    var sx2=(thisright-thisleft)/2+thisleft;
+    var sx1=(sx2-thisleft)/2+thisleft;
+    var sx3=sx2+sx1;
+
+  //  var sx6=sx5+sx4;
+   Swipe(sx1,thisbottom-100,sx1,thistop,600);
+   sleep(800) 
+   Swipe(sx2,thisbottom-100,sx2,thistop,800);
+   sleep(800)   
+   Swipe(sx3,thisbottom-100,sx3,thistop,700);
+   sleep(800);
+   Swipe(sx1-50,thisbottom-100,sx3,thistop,800);
+   sleep(800)
+   Swipe(sx3-50,thisbottom-100,sx1,thistop,800);
+   sleep(800)
+   Swipe(sx1-150,thisbottom-100,sx1-300,thistop,800);
+
+   sleep(800)
+   Swipe(160,2223,1271,2383,800);
+   sleep(800)
+   Swipe(160,2376,1240,2258,800);
+  }
+  sleep(5000);
+ thisforstart=false;
+}  
+
