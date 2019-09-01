9a070e0431155c9c893c14a9aef1255c2e57f11b
hubGeGe <1205939666@qq.com>
2019-09-01 17:48:45 +0800

    全民头条更该js文件

--- applist/全民头条.js
+++ applist/全民头条.js
@@ -1,10 +1,10 @@
  function finditem(){ 
             //1标识出主框架定界符
-            var ele=className(v4feature);
+            var ele=className("android.widget.ListView");
             //2定位到结构块层级父节点，并取出结构块数量
-            var subcount=ele.findOnce(0).child(0).child(0).child(0).child(0).child(0).childCount();
+            var subcount=ele.findOnce(0).childCount();
             //将主框架实例化
-            var main=ele.findOnce(0).child(0).child(0).child(0).child(0).child(0);
+            var main=ele.findOnce(0);
             for(var i=1;i<subcount;i++){
                   try{
                        var gg1=main.child(i).child(0).child(0).child(0).child(1).child(0);
