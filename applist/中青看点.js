2a6bc911d26df3bcbe2f20c81f92c05a940b83ad
ProbieMrX <648131813@qq.com>
2019-09-04 02:26:09 +0800

    修正并完善中青看点json文件和js文件

--- applist/中青看点.js
+++ applist/中青看点.js
@@ -1,83 +1,22 @@
-module.exports = {
-    //用于检索非广告的新闻条目
-    finditem:function(){
-
-
-    /******************************自定义部分开始********************************************/    
-            //中青看点        
-            //1标识出主框架定界符
-            var ele=className(v4feature);//.className("LinearLayout").findOnce(5);
-            //2定位到结构块层级父节点，并取出结构块数量
-            var subcount=ele.findOnce(0).child(0).child(0).child(0).child(0).child(0).childCount();// var subcount=ele.findOnce(0).child(0).child(0).childCount();
-            //检测一下子节点数量是否正确
-           // alert(subcount);exit();
-            //将主框架实例化
-            var main=ele.findOnce(0).child(0).child(0).child(0).child(0).child(0);
-            for(var i=0;i<subcount;i++){
-                try{
-                    //取出标题，主要是为了验证正确性
-                    var ltitle=main.child(i).child(0).child(0).text();
-                    
-                    try{
-                                if(main.child(i).child(2).child(2).text()=="立即下载"){
-                                play("global",i);
-                                play("global","广告不点击");
-                                //alert(ltitle);
-                            } 
-                    }catch(e){
-                       // alert(e);
-                       // 2 0 text 广告
-                       //1 1
-                       // 0 1 0
-                       try{        
-                           if(main.child(i).child(2).child(0).text().replace(/^\s+|\s+$/g,"")=="广告"||
-                                    main.child(i).child(1).child(1).text().replace(/^\s+|\s+$/g,"")=="广告"||
-                                    main.child(i).child(0).child(1).child(0).text().replace(/^\s+|\s+$/g,"")=="广告"){
-                                play("global",i);
-                                play("global","广告不点击");
-                                alert(ltitle); 
-                                    }else{
-                                        play("global",i);
-                                        play("global","点击");
-                                        return main.child(i);
-                                       // alert(ltitle);      
-                                    } 
-                                
-                        }catch(e){
-                            
-                        play("global",i);
-                        play("global","点击");
-                        return main.child(i);
-                       // alert(ltitle);    
-                       }
-                      
-    
-    
-                    }
-                   
-                    // if(main.child(i).child(2).id()=="tv_jingxuan"){
-                    
-                    //             play("global",i);
-                    //             play("global","广告不点击");
-                    //           //  return false;
-                    // }else{
-                    //         play("global",i);
-                    //         play("global","点击"); 
-                    //         return main.child(i);
-                    // }
-                        
-                }catch(e){
-            
-                //     play("global",i);
-                // play("global","广告不点击")
-                //return false;
-    
-                }
-            }
+function  finditem(){
+    var ele=className(v7feature);
+    var subcount=ele.findOnce(0).childCount();
+    var main=ele.findOnce(0);
+    for(var i=1;i<subcount;i++){
+        try{
+            var gg1=main.child(i).child(2).child(0).child(0);
+            if(gg1.text()=="广告 • 了解详情"){
+            play('global',i);
+            play('global','广告不点击'); 
             return false;
-    
-            //return false;
-
-/******************************自定义部分结束********************************************/ 
+                }
+            }catch(e){
+                } 
+        try{
+            play('global',i);
+            play('global','点击');
+            return main.child(i);
+            }catch(e){
+            }  
     }
 }
