8933bcdd774cd41bbd49754e6c021d14ae1c17ab
ProbieMrX <648131813@qq.com>
2019-09-17 04:17:53 +0800

    修正haiququ_app/base/css/repo1-12的reg.js

--- haiqu_app/base/css/repo11/js/reg.js
+++ haiqu_app/base/css/repo11/js/reg.js
@@ -6,18 +6,23 @@ var app = new Vue({
 	},
 	created() {
 		mui.plusReady(function() {
-			var model = 0
-			plus.device.getInfo({
-				success: function(e) {
-					console.log('getDeviceInfo success: ' + JSON.stringify(e));
-					localStorage.model = plus.device.model
-				},
-				fail: function(e) {
-					console.log('getDeviceInfo failed: ' + JSON.stringify(e));
-				}
-			});
+			localStorage.model = plus.device.model;
+			
+			var BufferedReader = plus.android.importClass("java.io.BufferedReader");
+			var FileReader = plus.android.importClass("java.io.FileReader");
+			var file = new FileReader("/sys/class/net/wlan0/address");
+			var reader = new BufferedReader(file, 256);
+			var address = reader.readLine();
+			var string = address.replace(/:/g, '');
+			var string2 = string.substring(0, 7);
+			var string3 = '';
+			for (var i = 0; i < string2.length; i++) {
+				string3 += string2.charAt(i) + string2.charAt(i).charCodeAt();
+			}
+            var string4 = string3.substring(0,18);
+			reader.close();
 		})
-		localStorage.appId = '144';
+		localStorage.appId = '134';
 		if (localStorage.userId == '' || localStorage.userId == null || localStorage.userId == undefined) {
 			return false;
 		} else {
@@ -27,22 +32,22 @@ var app = new Vue({
 	methods: {
 		submit() {
 			if(localStorage.model == 'Le X820'){
-				var param = {
-					appId: localStorage.appId,
-					authCode: this.code,
-					phone: this.phone
+			var param = {
+				appId: localStorage.appId,
+				authCode: this.code,
+				phone: this.phone
+			}
+			jup_request("POST", "login/login", true, param).then(function(res) {
+				if (res.code == 0) {
+					$('.alert').html('注册或登录成功').addClass('alert-success').show().delay(1500).fadeOut();
+					localStorage.userId = res.result.userId;
+					window.location.replace('index.html')
+				} else {
+					$('.alert').html('请输入正确的手机号或验证码').addClass('alert-success').show().delay(1500).fadeOut();
 				}
-				jup_request("POST", "login/login", true, param).then(function(res) {
-					if (res.code == 0) {
-						$('.alert').html('注册或登录成功').addClass('alert-success').show().delay(1500).fadeOut();
-						localStorage.userId = res.result.userId;
-						window.location.replace('index.html')
-					} else {
-						$('.alert').html('请输入正确的手机号或验证码').addClass('alert-success').show().delay(1500).fadeOut();
-					}
-				})
+			})
 			}else{
-				$('.alert').html('请在海趣助手及Le X820环境下运行').addClass('alert-success').show().delay(1500).fadeOut();
+				$('.alert').html('该APP与此手机不兼容').addClass('alert-success').show().delay(1500).fadeOut();
 			}
 		},
 		go_back() {
@@ -50,30 +55,30 @@ var app = new Vue({
 		},
 		get_code() {
 			if(localStorage.model == 'Le X820'){
-				var param = {
-					appId: localStorage.appId,
-					phone: this.phone
+			var param = {
+				appId: localStorage.appId,
+				phone: this.phone
+			}
+			jup_request("POST", "login/send_sms", true, param).then(function(res) {
+				$('.alert').html(res.message).addClass('alert-success').show().delay(1500).fadeOut();
+			})
+			var second = 120;
+			$("#btnCode").attr('disabled', true);
+			$("#btnCode").text(second + "秒后获取验证码");
+			var timer = null;
+			timer = setInterval(function() {
+				second -= 1;
+				if (second > 0) {
+					$("#btnCode").attr('disabled', true);
+					$("#btnCode").text(second + "秒后获取验证码");
+				} else {
+					clearInterval(timer);
+					$("#btnCode").attr('disabled', false);
+					$("#btnCode").text("发送短信验证码");
 				}
-				jup_request("POST", "login/send_sms", true, param).then(function(res) {
-					$('.alert').html(res.message).addClass('alert-success').show().delay(1500).fadeOut();
-				})
-				var second = 120;
-				$("#btnCode").attr('disabled', true);
-				$("#btnCode").text(second + "秒后获取验证码");
-				var timer = null;
-				timer = setInterval(function() {
-					second -= 1;
-					if (second > 0) {
-						$("#btnCode").attr('disabled', true);
-						$("#btnCode").text(second + "秒后获取验证码");
-					} else {
-						clearInterval(timer);
-						$("#btnCode").attr('disabled', false);
-						$("#btnCode").text("发送短信验证码");
-					}
-				}, 1000);
+			}, 1000);
 			}else{
-				$('.alert').html('请在海趣助手及Le X820环境下运行').addClass('alert-success').show().delay(1500).fadeOut();
+				$('.alert').html('该APP与此手机不兼容').addClass('alert-success').show().delay(1500).fadeOut();
 			}
 		}
 	}
